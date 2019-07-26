cf9d4453aa9c51689c899436f8df644edcd5fefd
anantharaman <anantharaman@hoovi.in>
2019-07-26 09:34:31 +0530

    added complete code

--- src/Utils/TestMain.java
+++ src/Utils/TestMain.java
@@ -8,7 +8,12 @@ public class TestMain {
     public static void main(String[] abcd){
         Hotel hotel = new Hotel(2,1,2);
         IChangeFloorMovement iChangeFloorMovement = new ChangeInFloorMovement();
-        iChangeFloorMovement.changeSubCorridorLightStatus(hotel.getFloorHashMap().get(1), 1, SensorInpStates.MOVEMENT);
+        iChangeFloorMovement.changeSubCorridorLightStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.MOVEMENT);
+        iChangeFloorMovement.changeSubCorridorAirConditionerStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.MOVEMENT);
+        System.out.print(hotel.getFloorHashMap().get(1).getFloorId());
+
+        iChangeFloorMovement.changeSubCorridorLightStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.NO_MOVEMENT);
+        iChangeFloorMovement.changeSubCorridorAirConditionerStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.NO_MOVEMENT);
         System.out.print(hotel.getFloorHashMap().get(1).getFloorId());
     }
 }
