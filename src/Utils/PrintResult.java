7a03791660e385f5f1267d29f452e92765980c6b
anantharaman <anantharaman@hoovi.in>
2019-07-27 11:02:11 +0530

    added output statements

---
+++ src/Utils/PrintResult.java
@@ -0,0 +1,34 @@
+package Utils;
+
+import NmAssets.Floor;
+import NmAssets.Hotel;
+import NmAssets.MainCorridor;
+import NmAssets.SubCorridor;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class PrintResult {
+
+    public void printResultHotel(Hotel hotel){
+        for(int i=1; i<=hotel.getFloorHashMap().size();i++){
+            Floor floor = hotel.getFloorHashMap().get(i);
+            System.out.println("FLoor No: " + floor.getFloorId());
+            Iterator<Map.Entry<Integer, MainCorridor>> mainCorridorItr = floor.getMainCorridorHashMap().entrySet().iterator();
+            while(mainCorridorItr.hasNext()){
+                Map.Entry<Integer, MainCorridor> entry = mainCorridorItr.next();
+                System.out.println(entry.getValue().getCorridorType() + "-" + entry.getValue().getCorridorId());
+                System.out.println("Light :  " + entry.getValue().getLight().getEquipmentState().toString());
+                System.out.println("Air Conditioner :" + entry.getValue().getAirConditioner().getEquipmentState().toString());
+            }
+
+            Iterator<Map.Entry<Integer, SubCorridor>> subCorridorItr = floor.getSubCorridorHashMap().entrySet().iterator();
+            while(subCorridorItr.hasNext()){
+                Map.Entry<Integer, SubCorridor> entry = subCorridorItr.next();
+                System.out.println(entry.getValue().getCorridorType() + "-" + entry.getValue().getCorridorId());
+                System.out.println("Light :  " + entry.getValue().getLight().getEquipmentState().toString());
+                System.out.println("Air Conditioner :" + entry.getValue().getAirConditioner().getEquipmentState().toString());
+            }
+        }
+    }
+}
--- src/Utils/TestMain.java
+++ src/Utils/TestMain.java
@@ -7,13 +7,16 @@ public class TestMain {
 
     public static void main(String[] abcd){
         Hotel hotel = new Hotel(2,1,2);
+        PrintResult printResult = new PrintResult();
         IChangeFloorMovement iChangeFloorMovement = new ChangeInFloorMovement();
         iChangeFloorMovement.changeSubCorridorLightStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.MOVEMENT);
         iChangeFloorMovement.changeSubCorridorAirConditionerStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.MOVEMENT);
-//        System.out.print(hotel.getFloorHashMap().get(1).getFloorId());
-
+        System.out.println("After movement ");
+        printResult.printResultHotel(hotel);
         iChangeFloorMovement.changeSubCorridorLightStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.NO_MOVEMENT);
         iChangeFloorMovement.changeSubCorridorAirConditionerStatus(hotel.getFloorHashMap().get(1), 2, SensorInpStates.NO_MOVEMENT);
-//        System.out.print(hotel.getFloorHashMap().get(1).getFloorId());
+        System.out.println("After No movement ");
+        printResult.printResultHotel(hotel);
+
     }
 }
